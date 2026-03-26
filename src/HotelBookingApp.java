0f892c029d89c320fe407ebefd83efbd5b09ca73
milantiju <milantiju@gmail.com>
2026-03-26 09:41:14 +0530

    UC1: Add welcome message module

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,7 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HotelBookingApp {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
     }
 }
